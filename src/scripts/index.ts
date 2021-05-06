8b597efec2c6304bb5603dabcbd6c7fdf45e623c
Justyna <jusbroniszewska@gmail.com>
2021-05-06 19:09:12 +0200

    💶 Add multicall

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -3,6 +3,7 @@ import { Wallet } from 'ethers'
 import { deployContract } from 'ethereum-waffle'
 import { CustomERC20__factory } from './tokens/CustomERC20__factory'
 import AMM from './AMM.json'
+import Multicall from './Multicall.json'
 
 async function start() {
   const port = 8545
@@ -14,12 +15,14 @@ async function start() {
   const token1 = await new CustomERC20__factory(wallet).deploy('token1', 'T1', 18, 1000)
 
   const amm = await deployContract(wallet, AMM, [token0.address, token1.address])
+  const multicall = await deployContract(wallet, Multicall, [])
 
   console.log({
     privateKey: privateKeys[0],
     token0: token0.address,
     token1: token1.address,
-    amm: amm.address
+    amm: amm.address,
+    multicall: multicall.address,
   })
 }
 
